4a154f39093884719537b37cc540b0979fa3339a
MatheusTrinca <matheusfelipetrinca@gmail.com>
2020-09-04 10:43:37 -0300

    parametros dinamicos e product detail data

--- controllers/admin.js
+++ controllers/admin.js
@@ -9,7 +9,7 @@ exports.getAddProducts = (req, res, next) =>{
 
 exports.postAddProducts = (req, res, next) => {
   const title = req.body.title;
-  const imageUrl = req.body.imageUrl === '' ? "/images/product-avatar.png" : req.body.imageUrl;
+  const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
   const product = new Product(title, imageUrl, description, price);
