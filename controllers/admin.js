c31e7bfe4fcab49d9467cb2ab1f3293b3de36d84
MatheusTrinca <matheusfelipetrinca@gmail.com>
2020-09-06 15:56:30 -0300

    edit product data

--- controllers/admin.js
+++ controllers/admin.js
@@ -13,7 +13,7 @@ exports.postAddProducts = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const product = new Product(title, imageUrl, description, price);
+  const product = new Product(null, title, imageUrl, description, price);
   product.save();
   res.redirect('/');
 }
@@ -37,8 +37,16 @@ exports.getEditProduct = (req, res, next) => {
   });
 }
 
-exports.posEditProduct = (req, res, next) => {
-  // Construct a new product and replace the existing one
+exports.postEditProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+  const updatedTitle = req.body.title;
+  const updatedImage = req.body.imageUrl;
+  const updatedPrice = req.body.price;
+  const updatedDescription = req.body.description;
+  const updatedProduct = new Product(prodId, updatedTitle, updatedImage, updatedDescription ,updatedPrice);
+  updatedProduct.save();
+  res.redirect('/admin/products');
+  
 }
 
 exports.getProducts = (req, res, next) => {
--- models/product.js
+++ models/product.js
@@ -5,38 +5,48 @@ const p = path.join(path.dirname(process.mainModule.filename), 'data', 'products
 
 const getProductsFromFile = callback => {
   fs.readFile(p, (err, fileContent) => {
-    if(err){
+    if (err) {
       callback([]);
-    }else{
+    } else {
       callback(JSON.parse(fileContent));
     }
   })
 }
 
 /* Classe Product */
-module.exports = class Product{
-  constructor(title, imageUrl, description, price){
+module.exports = class Product {
+  constructor(id, title, imageUrl, description, price) {
+    this.id = id;
     this.title = title;
     this.imageUrl = imageUrl;
     this.description = description;
     this.price = price;
   }
 
-  save(){
-    this.id = Math.random().toString();
+  save() {
     getProductsFromFile(products => {
-      products.push(this);
-      fs.writeFile(p, JSON.stringify(products), err => {
-        console.log(err);
-      })
-    })
+      if (this.id) {
+        const existingProductIndex = products.findIndex(p => p.id === this.id);
+        const updatedProducts = [...products];
+        updatedProducts[existingProductIndex] = this;
+        fs.writeFile(p, JSON.stringify(updatedProducts), err => {
+          console.log(err);
+        });
+      } else {
+        this.id = Math.random().toString();
+        products.push(this);
+        fs.writeFile(p, JSON.stringify(products), err => {
+          console.log(err);
+        });
+      }
+    });
   }
 
-  static fetchAll(callback){
+  static fetchAll(callback) {
     getProductsFromFile(callback);
   }
 
-  static findById(id, cb){
+  static findById(id, cb) {
     getProductsFromFile(products => {
       const product = products.find(p => p.id === id);
       cb(product);
